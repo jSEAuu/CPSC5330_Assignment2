b4ee71fd1367db75835a377ab446cb156db4553e
jSEAuu <134235334+jSEAuu@users.noreply.github.com>
2023-06-04 19:25:19 -0500

    Add files via upload

---
+++ CPSC5330_Assignment2/CPSC5330_Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC5330_Assignment2
+//
+//  Created by Lisa Mefford on 6/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
